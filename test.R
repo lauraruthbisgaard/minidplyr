c8a95b9736144b34bd564d8d5b5b7df09a8f9ded
Laura Ruth Bisgaard <au591815@uni.au.dk>
2025-08-21 10:08:52 +0200

    more add ons

--- test.R
+++ test.R
@@ -1,15 +1,23 @@
 #' Title
+#' 
+#' This will be 
+#' 
+#' This will be 2
+#' 
+#' This will be 3
+#' 
+#' @param x A number
+#' @param y A number
 #'
-#' @param x 
-#' @param y 
-#'
-#' @returns
+#' @returns The sum of x + y
 #' @export
 #'
 #' @examples
 test <- function(x,y){
     x + y
 }
+
+
 
 
 
